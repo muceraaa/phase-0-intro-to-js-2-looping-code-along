1d5f983504fc8075164a13a286199ff37b394225
valma <muceravn@gmail.com>
2022-06-02 17:09:12 +0300

    commit tree 6bb1ba84059ca1717856a5d40b223427d276e795

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let namesArray=["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+function writeCards(namesArray, event) {
+    let thankYou = [];
+for (let i = 0; i < namesArray.length; i++){
+    thankYou.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+}
+return thankYou;
+}
+function countDown(countDown){
+    while (countDown > 0){
+        console.log (countDown);
+        countDown -=1
+    }
+    console.log(countDown)
+}
